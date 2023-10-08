97b09c1c3057b4891e01bf22228a188e6854063e
Yparada <yucethpt@gmail.com>
2023-10-08 13:45:48 -0300

    Create basic api

--- src/app/api/task/route.tsx
+++ src/app/api/task/route.tsx
@@ -3,3 +3,7 @@ import { NextResponse } from "next/server";
 export function GET() {
     return NextResponse.json('Get task');
 }
+
+export function POST() {
+    return NextResponse.json('Create task');
+}
