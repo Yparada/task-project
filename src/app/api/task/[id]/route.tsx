043d7be94c0282aabfc93dbe435765857030464c
Yparada <yucethpt@gmail.com>
2023-10-08 15:50:27 -0300

    Create crud operations

--- src/app/api/task/[id]/route.tsx
+++ src/app/api/task/[id]/route.tsx
@@ -1,13 +1,36 @@
+import { prisma } from "@/libs/prisma";
 import { NextResponse } from "next/server";
 
-export function GET(request: any, { params }:any){
-    return NextResponse.json('Get task '+params.id)
+export async function GET(request: any, { params }: any) {
+    const task = await prisma.task.findUnique({
+        where: {
+            id: parseInt(params.id)
+        }
+    })
+    return NextResponse.json(task);
 }
 
-export function PUT(request: any, { params }:any){
-    return NextResponse.json('Update task '+params.id)
+export async function PUT(request: any, { params }: any) {
+    const data = await request.json();
+    const taskUpdated = await prisma.task.update({
+        where: {
+            id: parseInt(params.id)
+        },
+        data: data
+    })
+    return NextResponse.json(taskUpdated);
 }
 
-export function DELETE(request: any, { params }:any){
-    return NextResponse.json('Delete task '+params.id)
+export async function DELETE(request: any, { params }: any) {
+    try {
+        const taskRemoved = await prisma.task.delete({
+            where: {
+                id: parseInt(params.id)
+            }
+        });
+        return NextResponse.json(taskRemoved);
+    } catch (error: any) {
+        return NextResponse.json(error.message);
+
+    }
 }
