2c6c070e38a9f82fc87b5c59e8f58704d5b386dd
Yparada <yucethpt@gmail.com>
2023-10-09 00:02:54 -0300

    Update about page styles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,22 @@
 function AboutPage() {
     return (
-        <div>AboutPage</div>
-    )
+        <section className="container mx-auto">
+            <h3 className="text-4xl font-bold my-5">About</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ducimus,
+                commodi nostrum voluptates amet assumenda non dicta quo in, tenetur
+                molestiae vitae dolorem esse labore dolores! Quisquam, obcaecati ab! Est
+                tempora cupiditate aut soluta impedit eius? Quisquam eaque amet dolore
+                nemo nam sunt laborum debitis cupiditate expedita? Dolorum deserunt,
+                odit officia neque fugit id dolore, ut quaerat soluta, blanditiis
+                recusandae placeat cupiditate quidem repellat ex quod. Enim tempore hic
+                perferendis, unde iste incidunt dignissimos, asperiores iusto sint
+                eligendi est maxime consectetur itaque perspiciatis possimus rerum
+                excepturi, mollitia neque similique cumque? Vero iure voluptate fugiat
+                aliquam alias inventore, illo commodi delectus.
+            </p>
+        </section>
+    );
 }
 
-export default AboutPage
+export default AboutPage;
